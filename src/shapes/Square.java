8e94702240bc947e1749c6d1123a400d5c9d825d
sjwiles <sjwiles@csee137aer56.lcsee.wvu.edu>
2017-10-09 15:42:23 -0400

    Square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -8,13 +8,24 @@ public class Square extends Shape {
 	 * below.  Squares cannot have a negative side length.
 	 */
 	public Square(double side) throws IllegalArgumentException{
-		// TODO Implement me
+		if(side<0){
+			throw new IllegalArgumentException("Side can not be negative");
+		}
+		this.side=side;
 	}
 	
 	/*
 	 * Implement any remaining methods demanded by the superclass.
 	 */
 
+	
+	public double calculateArea(){
+		return this.getSide()*this.getSide();
+}
+	
+	
+	
+	
 	public double getSide() {
 		return side;
 	}
